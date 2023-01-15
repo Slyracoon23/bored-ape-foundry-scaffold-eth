bdb570da4f215dcdcdb8e01e686c57b4993064ff
Earl Potters <earl.potters@gmail.com>
2023-01-15 17:42:44 +0100

    chore: work

--- bored-ape-contracts/src/bored-app-proxy.sol
+++ bored-ape-contracts/src/bored-app-proxy.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzepplin/contracts/token/ERC721/IERC721.sol";
+import {IERC721} from "@openzepplin/contracts/token/ERC721/IERC721.sol";
 
-contract Counter {
+contract BoredAppProxy {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
