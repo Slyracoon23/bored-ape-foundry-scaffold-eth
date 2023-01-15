d37ea7896bf484243a94e6ae6a22580144948659
Earl Potters <earl.potters@gmail.com>
2023-01-15 18:52:40 +0100

    feat: add escrow sol

--- bored-ape-contracts/src/bored-ape-proxy.sol
+++ bored-ape-contracts/src/bored-ape-proxy.sol
@@ -3,14 +3,23 @@ pragma solidity ^0.8.13;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract Counter {
-    uint256 public number;
+contract Escrow {
+    mapping(address => mapping(address => uint256)) private nftHoldings;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    function withdrawlNFT(address token, uint256 tokenId) public {
+        require(
+            nftHoldings[msg.sender][address(token)] == tokenId,
+            "NFT not found in escrow"
+        );
+
+        IERC721(token).safeTransferFrom(address(this), msg.sender, tokenId);
+
+        delete nftHoldings[msg.sender][address(token)];
     }
 
-    function increment() public {
-        number++;
+    // Remember to first approve the contract to transfer the NFT
+    function depositNFT(address token, uint256 tokenId) public {
+        IERC721(token).safeTransferFrom(msg.sender, address(this), tokenId);
+        nftHoldings[msg.sender][address(token)] = tokenId;
     }
 }
