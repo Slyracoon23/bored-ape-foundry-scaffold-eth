a259b51796fc432088f5cd92f46620dae2ae9d1a
daviehall-kleenscape <122655143+daviehall-kleenscape@users.noreply.github.com>
2023-01-17 04:41:34 -0800

    Delete components/components directory

--- components/components/function/container/index.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-type ContainerProps = PropsWithChildren<{}>;
-
-export const Container = ({ children }: ContainerProps) => {
-  return <section className="flex items-center gap-1">{children}</section>;
-};
